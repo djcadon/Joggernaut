007213e8e06a25560d6b58c3105ede30d6e458c9
rai1975 <raihanrafeek@gmail.com>
2025-07-28 15:47:38 -0400

    new CRUD for friends and progress

--- src/db_queries/friends.py
+++ src/db_queries/friends.py
@@ -1,6 +1,46 @@
 from db_config import connect_db
 from db_queries.user_metrics import get_user_metrics
 
+def new_friend_connection(uid, fid):
+    cur, conn = connect_db()
+
+    try:
+        # See if it exists already
+        cur.execute('''
+                    SELECT * FROM friends
+                    WHERE (fid = %s AND uid = %s) OR (fid = %s AND uid = %s)
+                    ''', (fid, uid, uid, fid))
+
+        rows = cur.fetchall()
+
+        if len(rows) > 0:
+            return "Friendship already exists! Go hug"
+
+        else:
+            cur.execute('''
+                        INSERT INTO friends
+                        (uid, fid)
+                        VALUES (%s, %s)
+                        ''', (uid, fid))
+
+            conn.commit()
+
+            # Saving the inverse too, to avoid anomalies
+            cur.execute('''
+                        INSERT INTO friends
+                        (uid, fid)
+                        VALUES (%s, %s)
+                        ''', (fid, uid))
+
+            conn.commit()
+
+        cur.close()
+        conn.close()
+
+        return "Success"
+    except Exception as e:
+        return f"Error while adding friend connection: {e}"
+
 def get_all_friends(uid):
     cur, conn = connect_db()
 
@@ -36,3 +76,21 @@ def get_all_friends(uid):
 
     except Exception as e:
         return f"Error while fetching friends, {e}"
+
+def delete_friend_connection(uid, fid):
+    cur, conn = connect_db()
+
+    try:
+        cur.execute('''
+                    DELETE from friends
+                    WHERE (uid = %s AND fid = %s) OR (fid = %s AND uid = %s)
+                    ''', (uid, fid, fid, uid))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+        return "Success"
+
+    except Exception as e:
+        return f"Error while deleting friend connection: {e}"
---
+++ src/db_queries/progress.py
@@ -0,0 +1,79 @@
+from db_config import connect_db
+
+# CRUD for progress entries
+
+def new_progress_entry(uid, eid, wid, weight, reps, duration_mins):
+    cur, conn = connect_db()
+
+    try:
+        cur.execute('''
+                    INSERT INTO progress
+                    (uid, eid, wid, weight, reps, duration_mins)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                    ''', (uid, eid, wid, weight, reps, duration_mins))
+
+        conn.commit()
+
+
+        cur.close()
+        conn.close()
+        return "Success"
+
+    except Exception as e:
+        return f"Error while recording new progress entry: {e}"
+
+def edit_progress_entry(id, eid, wid, weight, reps, duration_mins):
+    cur, conn = connect_db()
+
+    try:
+        cur.execute('''
+                    UPDATE progress
+                    SET eid = %s, wid = %s, weight = %s, reps = %s, duration_mins = %s
+                    WHERE id = %s
+                    ''', (eid, wid, weight, reps, duration_mins, id))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+        return "Success"
+
+    except Exception as e:
+        return f"Error while updating progress record: {e}"
+
+def delete_progress_entry(id):
+    cur, conn = connect_db()
+
+    try:
+        cur.execute('''
+                    DELETE FROM progress
+                    WHERE id = %s
+                    ''', (id, ))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        return "Success"
+    except Exception as e:
+        return f"Error while deleting progress record: {e}"
+
+def get_progress_by_exercise(uid, eid):
+    cur, conn = connect_db()
+
+    try:
+        cur.execute('''
+                    SELECT * FROM progress
+                    WHERE uid = %s AND eid = %s
+                    ''', (uid, eid))
+
+        progress = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return progress
+
+    except Exception as e:
+        return f"Error when retrieving progress: {e}"
