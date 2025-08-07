0f9eeff13cb87ef8790baad5405c694bfb240ace
djcadon <caden1215@ipmolders.com>
2025-08-07 19:54:07 -0400

    CLI updated for tables and operations. Operations need to be coded

--- src/CLI.py
+++ src/CLI.py
@@ -12,7 +12,7 @@ print("""
 
      """)
 
-print("Available Tables:")
+print("Available Tables:\n")
 #Dictionary to map table numbers to names
 tables = {  
             1:"Users",
@@ -27,39 +27,49 @@ tables = {
 #Display available tables
 for key, value in enumerate(tables.items(), start=1):
     print(f"{key}. {value[1]}")
-print("Enter which table you want to interact with or 'exit' to quit:")
+print("\nEnter which table you want to interact with or 'exit' to quit:")
 #Function to handle user input for table selection
 def handle_tables():
     while True:
         user_input = input().strip().lower()
         match user_input:
-            case 1:
+            #Users table
+            case "1":
                print("You selected Users table.")
                return 1
-            case 2:
+            #User Metrics table
+            case "2":
                 print("You selected User Metrics table.")
                 return 2
-            case 3:
+            #Friends table
+            case "3":
                 print("You selected Friends table.")
                 return 3
-            case 4:
+            #Workouts table
+            case "4":
                 print("You selected Workouts table.")
                 return 4
-            case 5:
+            #Exercises table
+            case "5":
                 print("You selected Exercises table.")
                 return 5
-            case 6:
+            #Exercise_Workout table
+            case "6":
                 print("You selected Workout_Exercises table.")
                 return 6
-            case 7:
+            #Progress table
+            case "7":
                 print("You selected Progress table.")
                 return 7
-            case 8:
+            #Goals table
+            case "8":
                 print("You selected Goals table.")
                 return 8
+            #Exit the CLI
             case 'exit':
                 print("Exiting the CLI. Goodbye!")
                 exit(0)
+            #Invalid input
             case _:
                 print("Invalid input. Please try again.")
 selected_table = handle_tables()
@@ -97,20 +107,70 @@ operations = {
                 },
                 "Exercise_Workout":{
                     "SELECT":["SELECT *", "SELECT BY ID"],
-                    "INSERT":[],
+                    "INSERT":["WorkoutID", "ExerciseID"],
                     "UPDATE":["UPDATE BY ID"],
                     "DELETE":["DELETE BY ID"]
                 },
                 "Progress":{
                     "SELECT":["SELECT *", "SELECT BY ID"],
-                    "INSERT":[],
+                    "INSERT":["UserID", "ExerciseID", "WorkoutID", "Weight", "DurationMins"],
                     "UPDATE":["UPDATE BY ID"],
                     "DELETE":["DELETE BY ID"]
                 },
                 "Goals":{
                     "SELECT":["SELECT *", "SELECT BY ID"],
-                    "INSERT":[],
+                    "INSERT":["UserID", "GoalWeight"],
                     "UPDATE":["UPDATE BY ID"],
                     "DELETE":["DELETE BY ID"]
                 },
              }
+#Function to handle user input for operation selection
+def handle_operations(selected_table):
+    table_name = tables[selected_table]
+    print(f"Available operations for {table_name} table:")
+    for key, value in enumerate(operations[table_name].keys(), start=1):
+        print(f"{key}. {value}")
+    print("\nEnter which operation you want to perform or 'exit' to quit:")
+    while True:
+        user_input = input().strip().lower()
+        match user_input:
+            #Return to main menu
+            case "0":
+                print("You selected the main menu. Returning to table selection.")
+                return handle_tables()
+            #Select operation
+            case "1":
+                print(f"You selected SELECT operation on {table_name} table.\n")
+                print("Available SELECT options:")
+                for option in operations[table_name]["SELECT"]:
+                    print(f"{option}")
+                break
+            #Insert operation
+            case "2":
+                print(f"You selected INSERT operation on {table_name} table.\n")
+                print("INSERT SCHEMA:")
+                for option in operations[table_name]["INSERT"]:
+                    print(f"{option}")
+                break
+            #Update operation
+            case "3":
+                print(f"You selected UPDATE operation on {table_name} table.\n")
+                print("Available UPDATE options:")
+                for option in operations[table_name]["UPDATE"]:
+                    print(f"{option}")
+                break
+            #Delete operation
+            case "4":
+                print(f"You selected DELETE operation on {table_name} table.\n")
+                print("Available DELETE options:")
+                for option in operations[table_name]["DELETE"]:
+                    print(f"{option}")
+                break
+            #Exit the CLI
+            case 'exit':
+                print("Exiting the CLI. Goodbye!")
+                exit(0)
+            #Invalid input
+            case _:
+                print("Invalid input. Please try again.")
+handle_operations(selected_table)
