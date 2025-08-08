4ea499eaa5605e6a2db4e87fc2698a3e1919c17c
vinylotter <146021661+vinylotter@users.noreply.github.com>
2025-08-07 20:44:26 -0400

    Update user_metrics.py

--- src/db_queries/user_metrics.py
+++ src/db_queries/user_metrics.py
@@ -1,16 +1,18 @@
 from db_config import connect_db
 
+# Create a new entry in the user_metrics table
 def new_metric(height, weight, uid):
     cur, conn = connect_db()
 
     try:
+        # Insert the user's height and weight into the database
         cur.execute('''
                     INSERT INTO user_metrics
                     (uid, height, weight)
                     VALUES (%s, %s, %s)
                     ''', (uid, height, weight))
         
-        conn.commit()
+        conn.commit()  # Save the changes
         cur.close()
         conn.close()
 
@@ -19,16 +21,18 @@ def new_metric(height, weight, uid):
 
     return "Success, User metric created successfully"
 
+# Get the user's metrics (height and weight) using their user ID
 def get_user_metrics(uid):
     cur, conn = connect_db()
 
     try:
+        # Query the user_metrics table for the given user ID
         cur.execute('''
                     SELECT * FROM user_metrics
                     WHERE uid = %s
                     ''', (uid,))
 
-        rows = cur.fetchall()[0]
+        rows = cur.fetchall()[0]  # Get the first (and only) row
 
         return rows
 
