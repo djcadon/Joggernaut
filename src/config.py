1fb44a396be598e0803d7a006ed7cdb4833f84de
vinylotter <146021661+vinylotter@users.noreply.github.com>
2025-08-07 20:49:14 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,11 @@
+# Load environment variables from a .env file
 from dotenv import load_dotenv
 import os
 
+# Load the contents of the .env file into environment variables
 load_dotenv()
 
+# Retrieve database connection values from environment variables
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
